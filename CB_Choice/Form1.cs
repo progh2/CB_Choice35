1cf43addd274c6650e09ee6a4404e9444ac4ae8d
Gihun Ham <progh2@gmail.com>
2019-05-23 11:41:16 +0900

    #2 콤보박스 항목 추가, 선택, 버튼 클릭 코드 구현

--- CB_Choice/Form1.cs
+++ CB_Choice/Form1.cs
@@ -11,9 +11,65 @@ namespace CB_Choice
 {
     public partial class Form1 : Form
     {
+        string[] SList = new string[] {
+               "김밥", "샐러드김밥", "야채김밥",
+            "소고기김밥" ,"계란김밥", "라볶이"
+        };
+        string OrgStr = "";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for(int i = 0; i < this.SList.Length; i++)
+            {
+                this.cbList.Items.Add(this.SList[i]);
+            }
+            this.OrgStr = this.lblResult.Text;
+            if(this.SList.Length > 0)
+            {
+                this.cbList.SelectedIndex = 0;
+            }
+        }
+
+        private void CbList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.lblResult.Text = this.OrgStr + this.cbList.Text;
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            CheckInput();
+        }
+
+        private void TxtList_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == 13)
+            {
+                CheckInput();
+            }
+        }
+
+        private bool CheckInput()
+        {
+            if (this.txtList.Text == "")
+            {
+                MessageBox.Show("추가할 항목을 입력해주세요!", "알림",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                this.txtList.Focus();
+                return false;
+            }
+            else
+            {
+                this.cbList.Items.Add(this.txtList.Text);
+                this.txtList.Text = "";
+                this.cbList.SelectedIndex = this.cbList.Items.Count - 1;
+                this.txtList.Focus();
+                return true;
+            }
         }
     }
 }
